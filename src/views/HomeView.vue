0a114ee53715d211335e4d348dff09aaa256ad62
Villy-P <valeriuspetrini@gmail.com>
2023-09-08 14:56:02 -0500

    remove unessesary func

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -197,15 +197,6 @@
 		handleFileLoad(evt: ProgressEvent<FileReader>) {
 			this.currentFileURL = evt.target!.result! as string;
 		}
-
-		arrayBufferToBase64( buffer: ArrayBuffer ) {
-			let binary = '';
-			const bytes = new Uint8Array( buffer );
-			const len = bytes.byteLength;
-			for (let i = 0; i < len; i++)
-				binary += String.fromCharCode( bytes[ i ] );
-			return window.btoa( binary );
-		}
 	}
 </script>
 
